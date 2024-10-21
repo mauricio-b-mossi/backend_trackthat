c3ae4dca5f60ee43831cec8ba386886b4a458b92
mauricio.b.mossi <mauricio.b.mossi@gmail.com>
2024-10-20 20:03:51 -0400

    createddb

--- app/models.py
+++ app/models.py
@@ -1,5 +1,31 @@
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, SQLModel, create_engine
 
 class User(SQLModel, table=True):
     id : int | None = Field(primary_key=True, default=None)
     name : str
+    email : str
+    password : str
+
+class Application(SQLModel, table=True):
+    id : int | None = Field(primary_key=True, default=None) 
+    user_id : int = Field(default=None, foreign_key="user.id")
+    company : str
+    position : str | None
+    description : str | None
+    link : str | None
+    status : int
+    date : str
+
+class Notes(SQLModel, table=True):
+    id : int | None = Field(primary_key=True, default=None) 
+    application_id : int = Field(default=None, foreign_key="application.id")
+    description : str
+    date : str
+
+engine = create_engine("sqlite:///database.db", echo=True)
+
+def create_database(engine):
+    SQLModel.metadata.create_all(engine)
+
+if __name__ == "__main__":
+    create_database(engine)
